764e7f5d4e48f3f878443b484d51fa3b13455404
3Marz <omar490v@gmail.com>
2024-10-15 14:19:46 +0300

    adobt to new types

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,10 +1,11 @@
 import { animated, useInView } from "@react-spring/web"
 import TechIcon from "./TechIcon"
 import { SiPython, SiGit, SiCss3, SiJavascript, SiSqlite, SiTypescript, SiHtml5, SiReact, SiTailwindcss } from "react-icons/si";
+import { TechnologyType } from "../types";
 
 const iconSize = 60;
 
-const icons = [
+const icons: TechnologyType[] = [
 	{ name: "react", icon: <SiReact size={iconSize} />},
 	{ name: "Tailwind", icon: <SiTailwindcss size={iconSize} />},
 	{ name: "TypeScript", icon: <SiTypescript size={iconSize} />},
@@ -37,7 +38,7 @@ export default function Tech() {
 			<animated.h1 className="hover-sweep sweep-light w-fit" ref={ref} style={anim}>Tech I use:</animated.h1>
 			<div className="mx-[5vw] flex flex-wrap gap-x-11 gap-y-3">
 				{icons.map(({name, icon}, i) => {
-					return <TechIcon key={i} name={name}>{icon}</TechIcon>
+					return <TechIcon key={i} name={name} icon={icon} />
 				})}
 			</div>
 		</div>
