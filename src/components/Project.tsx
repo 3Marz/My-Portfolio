f00192a3f357c5314aef94f0c8057cc48f84b154
3Marz <omar490v@gmail.com>
2024-10-15 19:14:32 +0300

    replacing key with index for animation

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,15 +2,15 @@ import { animated, useInView } from "@react-spring/web"
 import { ProjectType } from "../types"
 
 type ProjectProps = ProjectType & {
-	key: number
+	index: number
 }
 
-export default function Project({name, desc, photo, techUsed, key}:ProjectProps) {
+export default function Project({name, desc, photo, techUsed, index}:ProjectProps) {
 	
 	const [ref, anim] = useInView(()=>({
 		from: {
 			opacity: 0,
-			y: key * 10
+			y: index * 10
 		},
 		to: {
 			opacity: 1,
@@ -18,12 +18,22 @@ export default function Project({name, desc, photo, techUsed, key}:ProjectProps)
 		},
 	}), {
 			rootMargin: '0% 0%',
-			once: true
 		})
 
 	return (
-		<animated.div ref={ref} style={anim}>
-			<img className="aspect-video bg-gray-200" src={photo}/>
+		<animated.div className="flex flex-col aspect-[4/3] space-y-2 p-2" ref={ref} style={anim}>
+			{/*<img className="aspect-video bg-gray-200" src={photo}/>*/}
+			<div className="aspect-video rounded-lg bg-gray-700"></div>
+
+			<p className="font-ibm-normal text-xl drop-shadow">
+				<b className="font-ibm-bold text-lg">{name} :</b> {desc}
+			</p>
+
+			<div className="flex items-center gap-4">
+				{techUsed?.map((tech, i)=> {
+					return <div key={i}>{tech.icon}</div>
+				})}
+			</div>
 		</animated.div>
 	)
 }
