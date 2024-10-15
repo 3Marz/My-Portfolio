3bfd649797d49bd1dbab9726c335c829ae01d42a
3Marz <omar490v@gmail.com>
2024-10-15 19:16:11 +0300

    adding some projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,38 @@
 import { useInView, animated } from "@react-spring/web"
 import { ProjectType } from "../types"
-import { SiTypescript } from "react-icons/si"
+import { SiHtml5, SiJavascript, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si"
 import Project from "./Project"
 
+const iconSize = 40;
+
 const projects : ProjectType[] = [
-	{ name:"term3d", 
+	{ 
+		name:"term3d", 
 		desc:"A 3d model viewer in the terminal", 
 		photo: undefined,
 		techUsed: [
-			{name:"Typescript", icon: <SiTypescript />}
-		]},
+			{name:"Typescript", icon: <SiTypescript size={iconSize} />}
+		]
+	},
+	{ 
+		name:"My Portfolio", 
+		desc:"made with react and react spring for animations", 
+		photo: undefined,
+		techUsed: [
+			{name:"React", icon: <SiReact size={iconSize}/>},
+			{name:"Typescript", icon: <SiTypescript size={iconSize}/>},
+			{name:"Tailwind", icon: <SiTailwindcss size={iconSize}/>}
+		]
+	},
+	{ 
+		name:"Chip-8 Emulator", 
+		desc:"an emulator for the chip-8 interpreted programming language", 
+		photo: undefined,
+		techUsed: [
+			{name:"Javascript", icon: <SiJavascript size={iconSize}/>},
+			{name:"Html", icon: <SiHtml5 size={iconSize}/>}
+		]
+	}
 ]
 
 export default function Projects() {
@@ -28,11 +51,11 @@ export default function Projects() {
 		})
 
 	return (
-		<div className="h-[100dvh] px-[15vw] mt-10 text-orange-400 prose-lg font-ibm-normal md:prose-xl lg:prose-2xl prose-h1:font-ibm-bold prose-p:font-ibm-normal">
-			<animated.h1 ref={ref} style={anim}>My Projects:</animated.h1>
-			<div className="flex">
+		<div className="h-[100dvh] px-[15vw] mt-10 space-y-8 text-orange-400">
+			<animated.h1 className="sweep-dark hover-sweep w-fit text-6xl font-ibm-bold" ref={ref} style={anim}>My Projects:</animated.h1>
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
 				{projects.map((project, i) => {
-					return <Project key={i} {...project}/>
+					return <Project key={i} index={i} {...project}/>
 				})}
 			</div>
 		</div>
