238014b90dda23e1f2a3f2129340a175ef7d1a85
3Marz <omar490v@gmail.com>
2024-10-16 19:07:23 +0300

    move projects to a constents file

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,39 +1,9 @@
 import { useInView, animated } from "@react-spring/web"
-import { ProjectType } from "../types"
-import { SiHtml5, SiJavascript, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si"
 import Project from "./Project"
 
-const iconSize = 40;
-
-const projects : ProjectType[] = [
-	{ 
-		name:"term3d", 
-		desc:"A 3d model viewer in the terminal", 
-		photo: undefined,
-		techUsed: [
-			{name:"Typescript", icon: <SiTypescript size={iconSize} />}
-		]
-	},
-	{ 
-		name:"My Portfolio", 
-		desc:"made with react and react spring for animations", 
-		photo: undefined,
-		techUsed: [
-			{name:"React", icon: <SiReact size={iconSize}/>},
-			{name:"Typescript", icon: <SiTypescript size={iconSize}/>},
-			{name:"Tailwind", icon: <SiTailwindcss size={iconSize}/>}
-		]
-	},
-	{ 
-		name:"Chip-8 Emulator", 
-		desc:"an emulator for the chip-8 interpreted programming language", 
-		photo: undefined,
-		techUsed: [
-			{name:"Javascript", icon: <SiJavascript size={iconSize}/>},
-			{name:"Html", icon: <SiHtml5 size={iconSize}/>}
-		]
-	}
-]
+import { projects } from "../constants/projects"
+import { IoIosArrowForward } from "react-icons/io"
+import { FaGithub, FaItchIo } from "react-icons/fa"
 
 export default function Projects() {
 
@@ -51,12 +21,22 @@ export default function Projects() {
 		})
 
 	return (
-		<div className="h-[100dvh] px-[15vw] mt-10 space-y-8 text-orange-400">
+		<div className="h-fit px-[15vw] mt-10 space-y-8 text-orange-400">
 			<animated.h1 className="sweep-dark hover-sweep w-fit text-6xl font-ibm-bold" ref={ref} style={anim}>My Projects:</animated.h1>
-			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
 				{projects.map((project, i) => {
 					return <Project key={i} index={i} {...project}/>
 				})}
+				<div className="h-full flex flex-col">
+					<a href="https://github.com/3Marz/" className="project hover:ring-2 ring-orange-400 h-full rounded-lg transition text-2xl min-h-24 font-ibm-bold flex justify-center items-center place-items-center">
+						More on my <FaGithub className="mx-2"/> github
+						<IoIosArrowForward size={28}/>
+					</a>
+					<a className="project hover:ring-2 ring-orange-400 h-full rounded-lg transition text-2xl min-h-24 font-ibm-bold flex justify-center items-center place-items-center">
+						More on my <FaItchIo className="mx-2" /> itch.io
+						<IoIosArrowForward size={28}/>
+					</a>
+				</div>
 			</div>
 		</div>
 	)
