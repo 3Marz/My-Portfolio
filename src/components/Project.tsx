e3da6d7fde7c3edf127e453f69a135b6758ed2a5
3Marz <omar490v@gmail.com>
2024-10-16 19:08:01 +0300

    done with the cards

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,9 +5,9 @@ type ProjectProps = ProjectType & {
 	index: number
 }
 
-export default function Project({name, desc, photo, techUsed, index}:ProjectProps) {
-	
-	const [ref, anim] = useInView(()=>({
+export default function Project({ url, name, desc, photo, techUsed, index }: ProjectProps) {
+
+	const [ref, anim] = useInView(() => ({
 		from: {
 			opacity: 0,
 			y: index * 10
@@ -17,26 +17,27 @@ export default function Project({name, desc, photo, techUsed, index}:ProjectProp
 			y: 0
 		},
 	}), {
-			rootMargin: '0% 0%',
-			once: true
-		})
+		once: true
+	})
 
 	return (
-		<animated.a className="project flex flex-col aspect-[4/3] pb-2 space-y-2 ring-orange-400 hover:ring-2 rounded-lg transition" ref={ref} style={anim}>
-			{/*<img className="aspect-video bg-gray-200" src={photo}/>*/}
-			<div className="project aspect-video rounded-lg bg-gray-700"></div>
+		<animated.a href={url} className="project flex flex-col pb-2 space-y-2 ring-orange-400 hover:ring-4 rounded-lg transition h-full w-fit" ref={ref} style={anim}>
+			
+			<div className="project h-fit aspect-video w-full overflow-hidden">
+				<img className="project rounded-lg h-full w-full" src={photo} />
+			</div>
 
-			<div className="project space-y-2 px-4">
-				<p className="project font-ibm-normal text-xl drop-shadow">
+			<div className="project space-y-2 px-4 flex flex-col justify-between">
+				<p className="project font-ibm-normal text-lg drop-shadow">
 					<b className="project font-ibm-bold text-lg">{name} :</b> {desc}
 				</p>
-
-				<div className="project flex items-center gap-4">
-					{techUsed?.map((tech, i)=> {
+				<div className="project flex gap-4">
+					{techUsed?.map((tech, i) => {
 						return <div key={i}>{tech.icon}</div>
 					})}
 				</div>
 			</div>
+
 		</animated.a>
 	)
 }
