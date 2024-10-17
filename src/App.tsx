36b810bb0e9d465b3f44e503721f060fd5dafc4d
3Marz <omar490v@gmail.com>
2024-10-17 12:33:49 +0300

    added mobile detction to hide the blob on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import { useTransition, animated, useTrail, SpringConfig } from '@react-spring/web';
 
 import Home from './pages/Home';
+import { useEffect } from 'react';
+import { isMobile } from './utils';
 
 const fast: SpringConfig = { tension: 1200, friction: 40 };
 const slow: SpringConfig = { mass: 10, tension: 200, friction: 50 };
@@ -16,13 +18,12 @@ function App() {
 		size: 1,
 		rad: "50%",
 		config: i == 0 ? fast : slow,
-		delay: 10
 	}));
 
 	const transitions = useTransition(location, {
-		from: { opacity: 0 },
-		enter: { opacity: 1 },
-		leave: { opacity: 0 }
+		from: { opacity: 0, scale: 2},
+		enter: { opacity: 1, scale: 1 },
+		leave: { opacity: 0, scale: 2 }
 	});
 
 	
@@ -41,17 +42,22 @@ function App() {
 			api.start({rad: "0%"})
 		}
 	}
-	window.addEventListener("mousemove", handleMouseMove)
+	useEffect(()=>{
+		// Needed to be in useEffect to prevent a bug when moving the mouse too early
+		if(!isMobile()) {
+			window.addEventListener("mousemove", handleMouseMove)
+		}
+	},[])
 		
-	return transitions((props, item) => (
-		<animated.div className="w-full" style={props}>
+	return transitions((transition, item) => (
+		<animated.div className="w-full" style={transition}>
 			<svg style={{ position: "absolute", width: 0, height: 0 }}>
 	      <filter id="goo">
 					<feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="25" />
 	        <feColorMatrix values={`1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -9`} />
 				</filter>
 			</svg>
-			<div className="hooksMain" >
+			<div className="hooksMain" hidden={isMobile()}>
 				{trail.map((props, index) => (
 					 <animated.span key={index} style={{ 
 						transform: props.xy.to(trans), 
